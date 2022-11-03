2ea7bd72dbacd55084cc941c2f01b89d18b55ae4
vandit-bera <vanditbera@gmail.com>
2022-11-03 17:59:59 +0530

    Add Details

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
             <div className="container">
-                <a className="navbar-brand" href="#page-top">Start Bootstrap</a>
+                <a className="navbar-brand" href="#page-top">Vandit Bera</a>
                 <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i className="fas fa-bars"></i>
